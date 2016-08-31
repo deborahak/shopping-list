5b9743e20bf42bdbba30f2113a74d079f8abc550
Deborah Kain <deborahkain@mac.com>
2016-08-30 19:41:55 -0700

    final

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
   $("#js-shopping-list-form").submit(function(event){
     event.preventDefault();
   
-  $(".shopping-list").append(
+  $('.shopping-list').append(
     '<li>' +
       '<span class="shopping-item">' + $("#shopping-list-entry").val() + '</span>' + 
       '<div class="shopping-item-controls">' + 
         '<button class="shopping-item-toggle">' +
           '<span class="button-label">check</span>' + 
         '</button>' +
+        '<button class="shopping-item-delete">'+'<span class="button-label">delete</span></button>'+
       '</div>' + 
     '</li>' 
 
@@ -21,7 +22,8 @@ $(".shopping-list").on("click", ".shopping-item-delete", function(event){
   $(this).closest("li").remove();
 });
 $(".shopping-list").on("click", ".shopping-item-toggle", function(event){
-  $(this).closest("li").find(".shopping-item").toggleClass("shopping-item_checked");
+  console.log("woo hoo")
+  $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
   
 })
